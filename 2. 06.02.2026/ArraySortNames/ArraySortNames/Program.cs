7f4dd6536d0e6f32da7bc2590ddb5254e3c26514
TTVHardCoreGamer <aaronvork22@gmail.com>
2026-02-06 09:32:18 +0200

    ArraySortNames

--- 2. 06.02.2026/ArraySortNames/ArraySortNames/Program.cs
+++ 2. 06.02.2026/ArraySortNames/ArraySortNames/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Kasutame Array ja Sort-i");
+
+            string[] animals = { "cat", "alligator", "fox",
+                "donkey", "bear", "elephant", "goat" };
+            
+            //tuleb kasutada foreachi ja näidata kõiki loomi
+            //paneb kõik tähestikulisse järjekorda
+            
+            //Array.Sort(animals);
+            foreach (string animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+            Console.WriteLine("--------------------------");
+            
+            //järjesta kolm esimest sõna tähestikulises järjestuses
+            //vaadake Sort meetodit ja mitu overload sellel on
+
+            Array.Sort(animals, 0, 3);
+            foreach (string animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+
+            Console.WriteLine("--------------------------");
+            
+            int[] numbers = { 1, 2, 3, 4, 3, 55, 23, 2 };
+            //tuleb välistada kordused
+            //mida teha, et numbrid ei korduks
+
+            int[] distinct = numbers.Distinct().ToArray();
+            foreach (int num in distinct)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
