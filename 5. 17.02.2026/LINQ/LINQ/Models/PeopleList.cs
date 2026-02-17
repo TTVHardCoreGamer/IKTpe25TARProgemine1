e5bdafc02c0eca9907188e69903b442d0935ccfd
TTVHardCoreGamer <aaronvork22@gmail.com>
2026-02-17 15:54:34 +0200

    LINQ

---
+++ 5. 17.02.2026/LINQ/LINQ/Models/PeopleList.cs
@@ -0,0 +1,11 @@
+﻿namespace LINQ
+{
+    public class PeopleList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Guid GenderId { get; set; }
+    }
+}
+
