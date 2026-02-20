57b3bc64b1f2c55b8118d4dfd944d4e97aa2cd2c
TTVHardCoreGamer <aaronvork22@gmail.com>
2026-02-20 09:52:41 +0200

    LINQ

---
+++ 6. 20.02.2026/LINQ/LINQ/PeopleList.cs
@@ -0,0 +1,58 @@
+﻿namespace LINQ
+{
+    public class PeopleList
+    {
+        public static readonly List<People> peoples = new List<People>
+        {
+            new People()
+            {
+                Id = 1,
+                Name = "Moona",
+                Age = 31,
+                GenderId = Guid.Parse("63a694f3-e80e-4df2-af2e-f35c5d973105")
+            },
+            new People()
+            {
+                Id = 2,
+                Name = "Poona",
+                Age = 21,
+                GenderId = Guid.Parse("bdf2fa86-d8dd-4548-bc98-3f15907b8e55")
+            },
+            new People()
+            {
+                Id = 3,
+                Name = "Ron",
+                Age = 18,
+                GenderId = Guid.Parse("bdf2fa86-d8dd-4548-bc98-3f15907b8e55")
+            },
+            new People()
+            {
+                Id = 4,
+                Name = "Anna",
+                Age = 20,
+                GenderId = Guid.Parse("63a694f3-e80e-4df2-af2e-f35c5d973105")
+            },
+            new People()
+            {
+                Id = 5,
+                Name = "Mari",
+                Age = 19,
+                GenderId = Guid.Parse("63a694f3-e80e-4df2-af2e-f35c5d973105")
+            },
+            new People()
+            {
+                Id = 6,
+                Name = "Mari",
+                Age = 21,
+                GenderId = Guid.Parse("63a694f3-e80e-4df2-af2e-f35c5d973105")
+            },
+            new People()
+            {
+                Id = 7,
+                Name = "Bill",
+                Age = 21,
+                GenderId = Guid.Parse("bdf2fa86-d8dd-4548-bc98-3f15907b8e55")
+            }
+        };
+    }
+}
---
+++ 6. 20.02.2026/LINQ/LINQ/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tuleb teha class nimega PeopleList
+            //Seal on kuus rida andmeid
+            //Kindlasti peab olema kaks Mari nimega isikut,
+            //aga erinevate vanustega
+
+            Console.WriteLine("Tee valik numbriga");
+            Console.WriteLine("1. ThenByLINQ");
+            Console.WriteLine("2. ThenByDescendingLINQ");
+            Console.WriteLine("3. SelectLINQ");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    ThenByLINQ();
+                    break;
+
+                case 2:
+                    ThenByDescendingLINQ();
+                    break;
+
+                case 3:
+                    SelectLINQ();
+                    break;
+
+                default:
+                    Console.WriteLine("Vale valik");
+                    break;
+            }
+        }
+
+        //kutsuda meetod switchis esile
+        public static void ThenByLINQ()
+        {
+            //thenby sorteerib numbrilises järjestuses
+            var thenByResult = PeopleList.peoples
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Age);
+
+            Console.WriteLine("ThenBy järgi sorteerimine");
+            foreach (var item in thenByResult)
+            {
+                Console.WriteLine(item.Name + " " + item.Age);
+            }
+        }
+
+        public static void ThenByDescendingLINQ()
+        {
+            var thenByResult = PeopleList.peoples
+                .OrderBy(x => x.Name)
+                .ThenByDescending(x => x.Age);
+
+            Console.WriteLine("ThenByDescending järgi sorteerimine");
+            foreach (var item in thenByResult)
+            {
+                Console.WriteLine(item.Name + " " + item.Age);
+            }
+        }
+
+        public static void SelectLINQ()
+        {
+            //select lihtsalt tagastab andmed nii nagu need on andmebaasis
+            //sama hea, mis SQL select
+            var result = PeopleList.peoples
+                .Select(x => new
+                { 
+                    Name = x.Name, 
+                    Age = x.Age,
+                });
+                
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Name + " " + item.Age);
+            }
+        }
+    }
+}
