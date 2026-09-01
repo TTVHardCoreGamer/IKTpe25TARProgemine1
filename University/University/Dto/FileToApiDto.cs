1e3b8c8ae7a9b9ba941ca32f01e857405b2d862a
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-01 13:22:08 +0300

    Delete

--- University/University/Dto/FileToApiDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace University.Dto
-{
-    public class FileToApiDto
-    {
-        public Guid Id { get; set; }
-        public string? ExsistingFilePath { get; set; }
-        public Guid? CourseId { get; set; }
-    }
-}
--- University/University/University/ViewModel/CourseVM/ImageViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace University.ViewModel.CourseVM
-{
-    public class ImageViewModel
-    {
-        public Guid ImageId { get; set; }
-        public string? FilePath { get; set; }
-    }
-}
